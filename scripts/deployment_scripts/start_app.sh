40035cca004068dd3439674161fc5b9360143769
Ben Thompson <splitoe@gmail.com>
2021-12-19 19:40:45 -0500

    update CodeDeploy config

--- scripts/deployment_scripts/start_app.sh
+++ scripts/deployment_scripts/start_app.sh
@@ -1,3 +1,5 @@
+cp /var/www/application/pi-sensor-v2/nginx/application.conf /etc/nginx/sites-available/application.conf
+cp /var/www/application/pi-sensor-v2/nginx/application.service /etc/systemd/system/application.service
 sudo systemctl enable application.service
 sudo systemctl restart nginx
 sudo systemctl restart application.service
